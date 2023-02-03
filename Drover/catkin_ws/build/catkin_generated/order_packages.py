26fc460c31a01fd7e4c9bf04c527c246d6bce655
“FANSAN6190” <fanin.s.pbl@gmail.com>
2023-02-03 23:32:14 +0530

    drover on ROS

---
+++ Drover/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/fansan/Desktop/Land-Rover/Drover/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/fansan/Desktop/Land-Rover/Drover/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/fansan/Desktop/Land-Rover/Drover/catkin_ws/devel;/opt/ros/noetic' != '' else []
